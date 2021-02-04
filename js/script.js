ad81c3630b28c1374d9581874065302ec9011810
timmit147 <timmeeuwsen@hotmail.nl>
2021-02-04 13:32:04 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,87 @@
-
+// time
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-window.onload = function() {
-  document.getElementById("overlay").style.left = "100%";
+// // page trantsision
+// window.onload = function() {
+//   document.getElementById("overlay").style.left = "100%";
+// }
+
+// async function link(url) {
+//   	document.getElementById("overlay").style.left = "0";
+//   	await sleep(1800);
+//   	window.location.href = url;
+// }
+
+
+// Filter
+function selectall(){
+document.querySelector("#filter-all").classList.toggle("bold");
+document.querySelector("#filter-vormgeving").classList.remove("bold");
+document.querySelector("#filter-progameren").classList.remove("bold");
+
+
+var list = document.querySelectorAll("section");
+for (var i = 0; i < 999; ++i) {
+	if ( list[i] == undefined){
+		return;
+	}
+		list[i].classList.remove('hide');
+   
+}
 }
 
-async function link(url) {
-  	document.getElementById("overlay").style.left = "0";
-  	await sleep(1800);
-  	window.location.href = url;
+
+function vormgeving(){
+
+document.querySelector("#filter-vormgeving").classList.toggle("bold");
+document.querySelector("#filter-all").classList.remove("bold");
+document.querySelector("#filter-progameren").classList.remove("bold");
+
+var list = document.querySelectorAll("section");
+	for (var i = 0; i < 999; ++i) {
+		if ( list[i] == undefined){
+			return;
+		}
+		else{
+			if(list[i].classList.contains("vormgeving")){
+				list[i].classList.remove('hide');
+			}
+			else{
+				list[i].classList.add('hide');
+			}
+			
+		}
+
+	   
+	}
+
+}
+
+function progameren(){
+
+	document.querySelector("#filter-progameren").classList.toggle("bold");
+document.querySelector("#filter-all").classList.remove("bold");
+document.querySelector("#filter-vormgeving").classList.remove("bold");
+
+var list = document.querySelectorAll("section");
+	for (var i = 0; i < 999; ++i) {
+		if ( list[i] == undefined){
+			return;
+		}
+		else{
+			if(list[i].classList.contains("progameren")){
+				list[i].classList.remove('hide');
+			}
+			else{
+				list[i].classList.add('hide');
+			}
+			
+		}
+
+	   
+	}
+
+
 }
